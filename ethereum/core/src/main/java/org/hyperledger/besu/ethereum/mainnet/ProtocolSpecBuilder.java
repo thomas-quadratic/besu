eec89f715ef7bb1dc3fb027bf82294c7515e14e2
Luis Pinto <luis.pinto@consensys.net>
2025-10-08 15:38:02 +0100

    spotless and show opcode optimization mode in user overview
    Signed-off-by: Luis Pinto <luis.pinto@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/ProtocolSpecBuilder.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/ProtocolSpecBuilder.java
@@ -330,7 +330,7 @@ public class ProtocolSpecBuilder {
     final GasLimitCalculator gasLimitCalculator =
         gasLimitCalculatorBuilder.apply(feeMarket, gasCalculator, blobSchedule);
     final EVM evm = evmBuilder.apply(gasCalculator, evmConfiguration);
-    LOGGER.info(
+    LOGGER.debug(
         "Opcode optimizations {} for milestone {}",
         evm.getEvmConfiguration().enableOptimizedOpcodes() ? "enabled" : "disabled",
         hardforkId);
